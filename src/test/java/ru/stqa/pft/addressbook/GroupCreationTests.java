de5873a17b6f3c3a718dce2e516c0ff8c4222c42
aibukrid <115988571+aibukrid@users.noreply.github.com>
2022-11-11 21:07:39 +0300

    Создали общий базовый класс, добавили новый тест по удалению группы

--- src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -12,68 +12,15 @@ import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
-public class GroupCreationTests {
-    private WebDriver driver;
-    private Map<String, Object> vars;
-    JavascriptExecutor js;
-
-    @BeforeMethod
-    public void setUp() {
-        driver = new ChromeDriver();
-        js = (JavascriptExecutor) driver;
-        vars = new HashMap<String, Object>();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        login("admin", "secret");
-    }
-
-    private void login(String username, String password) {
-        driver.get("http://localhost/addressbook/group.php");
-        driver.findElement(By.name("user")).click();
-        driver.findElement(By.name("user")).sendKeys(username);
-        driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).sendKeys(password);
-        driver.findElement(By.cssSelector("input:nth-child(7)")).click();
-    }
+public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() {
-        gotoGroupPage(By.linkText("groups"));
+        gotoGroupPage();
         initGroupCreation();
-        fillGroupForm(new GroupData("test1", "test2", "test3"));
-        submitGroupCreation(By.name("submit"));
-        returnToGroupPage(By.linkText("group page"));
-    }
-
-    private void gotoGroupPage(By groups) {
-        driver.findElement(groups).click();
-    }
-
-    private void initGroupCreation() {
-        driver.findElement(By.name("new")).click();
-    }
-
-    private void fillGroupForm(GroupData groupData) {
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).sendKeys(groupData.name());
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).sendKeys(groupData.header());
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).sendKeys(groupData.footer());
-    }
-
-    private void submitGroupCreation(By submit) {
-        driver.findElement(submit).click();
-    }
-
-    private void returnToGroupPage(By group_page) {
-        driver.findElement(group_page).click();
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
+        fillGroupForm("test1", "test2", "test3");
+        submitGroupCreation();
+        returnToGroupPage();
     }
 
 }
---
+++ src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -0,0 +1,77 @@
+package ru.stqa.pft.addressbook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestBase {
+    private WebDriver driver;
+    private Map<String, Object> vars;
+    JavascriptExecutor js;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+        js = (JavascriptExecutor) driver;
+        vars = new HashMap<String, Object>();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+        driver.get("http://localhost/addressbook/");
+        driver.findElement(By.name("user")).click();
+        driver.findElement(By.name("user")).sendKeys(username);
+        driver.findElement(By.name("pass")).click();
+        driver.findElement(By.name("pass")).sendKeys(password);
+        driver.findElement(By.cssSelector("input:nth-child(7)")).click();
+    }
+
+    public void gotoGroupPage() {
+        driver.findElement(By.linkText("groups")).click();
+    }
+
+    public void initGroupCreation() {
+        driver.findElement(By.name("new")).click();
+    }
+
+    public void fillGroupForm(String name, String header, String footer) {
+        driver.findElement(By.name("group_name")).click();
+        driver.findElement(By.name("group_name")).sendKeys(name);
+        driver.findElement(By.name("group_header")).click();
+        driver.findElement(By.name("group_header")).sendKeys(header);
+        driver.findElement(By.name("group_footer")).click();
+        driver.findElement(By.name("group_footer")).sendKeys(footer);
+    }
+
+    public void submitGroupCreation() {
+        driver.findElement(By.name("submit")).click();
+    }
+
+    public void returnToGroupPage() {
+        driver.findElement(By.xpath("//a[@href='group.php']")).click();
+    }
+
+    public void selectGroups() {
+        driver.findElement(By.name("selected[]")).click();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+
+    public void deletionGroups() {
+        driver.findElement(By.name("delete")).click();
+    }
+
+}
