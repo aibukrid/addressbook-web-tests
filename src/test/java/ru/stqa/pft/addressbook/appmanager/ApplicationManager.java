422453f7f68afbdbbaf4bc82341aac1c45755d67
aibukrid <115988571+aibukrid@users.noreply.github.com>
2022-11-14 17:11:01 +0300

    Расщепление и специализация. Выделяем специализированные классы помощники.
    Раздробили ApplicationManager на несколько более простых классов GroupHelper, Navigation, Session, и раскидали вспомогательные функции по этим новым классам.

--- src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,75 +1,44 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
 
 public class ApplicationManager {
+
     public WebDriver driver;
-    public Map<String, Object> vars;
-    JavascriptExecutor js;
+
+    private GroupHelper groupHelper;
+    private NavigationHelper navigationHelper;
+    private SessionHelper sessionHelper;
 
     public void init() {
         driver = new ChromeDriver();
-        js = (JavascriptExecutor) driver;
-        vars = new HashMap<String, Object>();
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        login("admin", "secret");
-    }
-
-    public void login(String username, String password) {
         driver.get("http://localhost/addressbook/");
-        driver.findElement(By.name("user")).click();
-        driver.findElement(By.name("user")).sendKeys(username);
-        driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).sendKeys(password);
-        driver.findElement(By.cssSelector("input:nth-child(7)")).click();
-    }
-
-    public void gotoGroupPage() {
-        driver.findElement(By.linkText("groups")).click();
-    }
-
-    public void initGroupCreation() {
-        driver.findElement(By.name("new")).click();
-    }
-
-    public void fillGroupForm(GroupData groupData) {
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-    }
-
-    public void submitGroupCreation() {
-        driver.findElement(By.name("submit")).click();
-    }
-
-    public void returnToGroupPage() {
-        driver.findElement(By.xpath("//a[@href='group.php']")).click();
-    }
-
-    public void selectGroup() {
-        driver.findElement(By.name("selected[]")).click();
-    }
-
-    public void deleteSelectedGroups() {
-        driver.findElement(By.name("delete")).click();
+        groupHelper = new GroupHelper(driver);
+        navigationHelper = new NavigationHelper(driver);
+        sessionHelper = new SessionHelper(driver);
+        sessionHelper.login("admin", "secret");
     }
 
     public void stop() {
         driver.quit();
     }
 
-}
+    public NavigationHelper getNavigationHelper() {
+        return navigationHelper;
+    }
 
+    public GroupHelper getGroupHelper() {
+        return groupHelper;
+    }
+
+    public SessionHelper getSessionHelper() {
+        return sessionHelper;
+    }
+
+}
---
+++ src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class NavigationHelper {
+
+    private final WebDriver driver;
+
+    public NavigationHelper(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void gotoGroupPage() {
+        driver.findElement(By.linkText("groups")).click();
+    }
+
+}
